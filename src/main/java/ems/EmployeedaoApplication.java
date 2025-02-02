5e27cb0287e7470368fee5f6700395c0977cd3c2
Khadhar Amir <35258931+AMIRKHADHER@users.noreply.github.com>
2025-02-03 00:00:57 +0100

    Update EmployeedaoApplication.java

--- src/main/java/ems/EmployeedaoApplication.java
+++ src/main/java/ems/EmployeedaoApplication.java
@@ -2,9 +2,10 @@ package ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeedaoApplication {
+public class EmployeedaoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeedaoApplication.class, args);
